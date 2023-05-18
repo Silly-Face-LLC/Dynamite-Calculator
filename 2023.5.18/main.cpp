c022cd76f89c38b904fc61bb0c7dc15ca49577c8
TermiNateVader <nathanbenstevens@gmail.com>
2023-05-18 15:44:31 -0400

    Create main.cpp

---
+++ 2023.5.18/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#define PI 3.14159265
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	double a;
+	double b;
+	double ans;
+	char ops;
+	//string a;
+	//string b;
+	//stringstream ss;
+	cout << "Dynamite Calculator \n \n";
+	//string ops;
+	while (true) {
+		/*getline(cin, a);
+		getline(cin, ops);
+		getline(cin, b);
+		ss << a;
+		ss << ops;
+		ss << b;
+		char ops = ops;*/
+		cin >> a;
+		cin >> ops;
+		
+			
+
+			switch (ops)
+			{
+			case '+':
+				cin >> b;
+				ans = a + b;
+				cout << ans << endl;
+				break;
+			case '-':
+				cin >> b;
+				ans = a + b;
+				cout << ans << endl;
+				break;
+			case '*':
+				cin >> b;
+				ans = a + b;
+				cout << ans << endl;
+				break;
+			case '/':
+				cin >> b;
+				ans = a / b;
+				cout << ans << endl;
+				break;
+			case '^':
+				cin >> b;
+				ans = pow(a, b);
+				cout << ans << endl;
+				break;
+			case '%':
+				ans = a / 100;
+				cout << ans << endl;
+			case '#':
+				ans = sqrt(a);
+				cout << ans << endl;
+			case 'c':
+				ans = cos(a*PI/180);
+				cout << ans << endl;
+			case 's':
+				ans = sin(a * PI / 180);
+				cout << ans << endl;
+			case 't':
+				ans = tan(a * PI / 180);
+				cout << ans << endl;
+
+			}
+			
+			
+		
+		
+	}
+}
